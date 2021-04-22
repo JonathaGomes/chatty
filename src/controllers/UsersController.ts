a860a9bba8067094564d33fee18257f5c2277da3
Jonatha Gomes <jonatha.dev.js@gmail.com>
2021-04-22 17:53:28 -0300

    Terceira aula

---
+++ src/controllers/UsersController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { UsersServices } from "../services/UsersService";
+
+class UsersController {
+  async create(request: Request, response: Response): Promise<Response> {
+    const { email } = request.body;
+    
+    const usersServices = new UsersServices();
+
+    const user = await usersServices.create(email);
+
+    return response.json(user);
+    
+  
+  }
+}
+
+export { UsersController }
